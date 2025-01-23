93b2a111b404300f7ac4f572cadbec8d0dfa8ffd
pranay65 <family.yerram@gmail.com>
2025-01-23 17:55:03 +0530

    Made User API

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,28 @@
-const exp=require('express')
-require("dotenv").config()
-const app=exp()
-app.use(exp.json())
-const port=process.env.PORT
-const client=require("mongodb").MongoClient
-client.connect(process.env.DBURL).then((client)=>{
-    const database=client.db("softwareDB")
-    const usersCollection=database.collection("users")
-    const issueCollection=database.collection("issues")
-    const projectsCollection=database.collection("projects")
-    app.set("usersCollection",usersCollection)
-    app.set("issueCollection",issueCollection)
-    app.set("projectsCollection",projectsCollection)
-    console.log("database connected")
+const exp = require("express");
+const userApp = require("./APIs/userApp");
+require("dotenv").config();
+const app = exp();
+app.use(exp.json());
+const port = process.env.PORT;
+const client = require("mongodb").MongoClient;
+client
+  .connect(process.env.DBURL)
+  .then((client) => {
+    const database = client.db("softwareDB");
+    const usersCollection = database.collection("users");
+    const issueCollection = database.collection("issues");
+    const projectsCollection = database.collection("projects");
+    app.set("usersCollection", usersCollection);
+    app.set("issueCollection", issueCollection);
+    app.set("projectsCollection", projectsCollection);
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
+app.use("/user", userApp);
 
-}).catch((err)=>{
-    console.log(err)
-    
-})
-
-
-
-
-
-app.listen(port,()=>{
-    console.log("running")
-})
-
+app.listen(port, () => {
+  console.log("running");
+});
