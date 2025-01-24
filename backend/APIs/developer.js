4a74c9f0feb5123c694272a831fa2fbbd6f70a38
pranay65 <family.yerram@gmail.com>
2025-01-24 19:34:13 +0530

    Minor fixes

--- backend/APIs/developer.js
+++ backend/APIs/developer.js
@@ -3,28 +3,25 @@ const expHandler = require("express-async-handler");
 const { verifyToken, isDev } = require("../Middleware/auth");
 const devApp = exp.Router();
 
-const getProjects=expHandler(async(req,res)=>{
-    const username=req.headers.curr_user_name
-    const usersCollection=req.app.get("usersCollection")
-    const projectsCollection=req.app.get("projectsCollection")
-    const dbuser=await usersCollection.findOne({name:username})
-    const projectsId=dbuser.projects
-    let resArr=[]
-    for (let index = 0; index < projectsId.length; index++) {
-        const element = projectsId[index];
-        const project= await projectsCollection.findOne({_id:element})
-        resArr.push(project)
-        
-    }
-    if (resArr) {
-        return res.send({ status: 200, payload: resArr });
-      } else {
-        return res.send({ status: 200, payload: [] });
-      }
-    
-})
+const getProjects = expHandler(async (req, res) => {
+  const username = req.headers.curr_user_name;
+  const usersCollection = req.app.get("usersCollection");
+  const projectsCollection = req.app.get("projectsCollection");
+  const dbuser = await usersCollection.findOne({ name: username });
+  const projectIDs = dbuser.projects;
+  let resArr = [];
+  for (let index = 0; index < projectIDs.length; index++) {
+    const reqID = projectIDs[index];
+    const project = await projectsCollection.findOne({ _id: reqID });
+    resArr.push(project);
+  }
+  if (resArr) {
+    return res.send({ status: 200, payload: resArr });
+  } else {
+    return res.send({ status: 200, payload: [] });
+  }
+});
 
-devApp.get("/",getProjects)
+devApp.get("/", verifyToken, isDev, getProjects);
 
-
-module.exports=devApp
+module.exports = devApp;
